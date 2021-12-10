553bf4916b98f28191c248578407a20ebef09dd7
Ilya Sokolov <inthecatsdreams@vk.com>
2021-12-10 10:22:27 +0100

    small tweak

--- main.c
+++ main.c
@@ -8,10 +8,11 @@
 #include "renderer.h"
 
 #define SWITCH_MODE_DELAY 3000000
-#define FPS_TIMER_TICK    1000000
-#define NUM_MODES         2
+#define FPS_TIMER_TICK 1000000
+#define NUM_MODES 2
 
-enum {
+enum
+{
 	INTEGER_FPS,
 	NO_FPS
 };
@@ -27,32 +28,43 @@ uint64_t t_tick;
 
 SceCtrlData pad;
 
-int sceDisplaySetFrameBuf_patched(const SceDisplayFrameBuf *pParam, int sync) {
+int sceDisplaySetFrameBuf_patched(const SceDisplayFrameBuf *pParam, int sync)
+{
 	sceCtrlPeekBufferPositive(0, &pad, 1);
-	if (pad.buttons & SCE_CTRL_START){
-		if (switch_tick == 0) switch_tick = sceKernelGetProcessTimeWide();
-		else if ((sceKernelGetProcessTimeWide() - switch_tick) > SWITCH_MODE_DELAY){
+	if (pad.buttons & SCE_CTRL_START)
+	{
+		if (switch_tick == 0)
+			switch_tick = sceKernelGetProcessTimeWide();
+		else if ((sceKernelGetProcessTimeWide() - switch_tick) > SWITCH_MODE_DELAY)
+		{
 			switch_tick = 0;
 			t_tick = 0;
 			tick = 0;
 			frames = 0;
 			mode = (mode + 1) % NUM_MODES;
 		}
-	}else switch_tick = 0;
-	switch (mode) {
+	}
+	else
+		switch_tick = 0;
+	switch (mode)
+	{
 	case INTEGER_FPS:
 		t_tick = sceKernelGetProcessTimeWide();
 		updateFramebuf(pParam);
-		if (tick == 0){
+		if (tick == 0)
+		{
 			tick = t_tick;
 			setTextColor(0x0066FF00);
-		}else{
-			if ((t_tick - tick) > FPS_TIMER_TICK){
+		}
+		else
+		{
+			if ((t_tick - tick) > FPS_TIMER_TICK)
+			{
 				fps = frames;
 				frames = 0;
 				tick = t_tick;
 			}
-			drawStringF(5, 5, "FPS: %d", fps);
+			drawStringF(5, 5, "%d", fps);
 		}
 		frames++;
 		break;
@@ -60,26 +72,27 @@ int sceDisplaySetFrameBuf_patched(const SceDisplayFrameBuf *pParam, int sync) {
 		break;
 	}
 	return TAI_CONTINUE(int, display_ref, pParam, sync);
-}	
+}
 
-void _start() __attribute__ ((weak, alias ("module_start")));
-int module_start(SceSize argc, const void *args) {
-	
+void _start() __attribute__((weak, alias("module_start")));
+int module_start(SceSize argc, const void *args)
+{
+
 	// Hooking sceDisplaySetFrameBuf
 	hook = taiHookFunctionImport(&display_ref,
-						TAI_MAIN_MODULE,
-						TAI_ANY_LIBRARY,
-						0x7A410B64,
-						sceDisplaySetFrameBuf_patched);
-						
+								 TAI_MAIN_MODULE,
+								 TAI_ANY_LIBRARY,
+								 0x7A410B64,
+								 sceDisplaySetFrameBuf_patched);
+
 	return SCE_KERNEL_START_SUCCESS;
 }
 
-int module_stop(SceSize argc, const void *args) {
+int module_stop(SceSize argc, const void *args)
+{
 
 	// Freeing hook
 	taiHookRelease(hook, display_ref);
 
 	return SCE_KERNEL_STOP_SUCCESS;
-	
 }
